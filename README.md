dde8aaf90fce7151c9fb48f401ae60a16192c297
Bill Motyl <bill.motyl@gmail.com>
2013-07-21 12:36:39 -0400

    add rest of class methods
    still need to be tested, commented

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 
 ## Crypto
 
-This project provides a CryptoString class that was developed for use in Coursera's Cryptotography I course.
+This project provides the CryptoString class that was developed for use in Coursera's Cryptotography I course.
 
-In order to abide with Coursera's honor code, no actual problems from the course are adressed here.  This is merely a utility class providing a small subset of functionalities found in PyCrypto to aid solving the problems so that the problems could be solved in Scala.
+In order to abide with Coursera's honor code, no actual problems from the course are included or addressed here.
+This is merely a utility class to aid in solving the problems in Scala.  It proves a small subset of the functionalities to be found, e.g.in PyCrypto.
  
 
 
--- src/main/scala/motylwg/crypto/CryptoString.scala
+++ src/main/scala/motylwg/crypto/CryptoString.scala
@@ -4,13 +4,52 @@ import java.math.BigInteger
 import scala.BigInt
 import java.security.MessageDigest
 
-case class CryptoString(val bytes: List[Byte]) {
+case class CryptoString(val bytes: Array[Byte]) {
 
   def toBytes() = bytes
 
   def length() = bytes.length
 
-  def byteToHex(b: Byte) : String = {
+  lazy val bigint = new BigInteger(bytes)
+
+  def inc = {
+    new CryptoString((bigint.add(BigInteger.ONE)).toByteArray)
+  }
+
+  def ^ (that: CryptoString) = {
+    val pairs = bytes.zip(that.toBytes())
+    val xored = pairs.map {p => (p._1 ^ p._2).toByte}
+
+    new CryptoString(xored.toList.toArray)
+  }
+
+  def + (that: CryptoString) = {
+    val pairs = bytes.zip(that.toBytes())
+    val xored = pairs.map {p => (p._1 ^ p._2).toByte}
+
+    CryptoString.fromHex(this.toString() + that.toString())
+  }
+
+  def toBlocks(blockSize: Int = 16): Array[CryptoString] = {
+    val n = (bytes.length - 1) / blockSize + 1
+    val lastn = bytes.length % blockSize
+    val hex = toString()
+
+    val blocks = for (i <- 0 until n) yield
+      if (i < n - 1) CryptoString.fromHex(hex.substring(2 * blockSize * i, 2 * blockSize * (i + 1)))
+      else CryptoString.fromHex(hex.substring(2 * blockSize * i))
+
+    blocks.toArray
+  }
+
+  def pad() = {
+    val list = bytes.toList
+    val npad = 16 - list.length % 16
+    val padded = list ++ (1 to npad).map {_ => npad.toByte}
+    new CryptoString(padded.toArray)
+  }
+
+  private def byteToHex(b: Byte) : String = {
     val bb = (b.toInt + 256) % 256
     val s = bb.formatted("%h")
     if (s.length == 1) "0" + s else s
@@ -19,6 +58,10 @@ case class CryptoString(val bytes: List[Byte]) {
   def toHex() = {
     val strings = bytes.map {b => byteToHex(b)}
     strings.mkString
+  }
+
+  def sha256() = {
+    new CryptoString(CryptoString.md.digest(bytes))
   }
 
   override def toString() = {
@@ -32,7 +75,7 @@ object CryptoString {
   def apply(s: String)  = {
     val bytes = for (c <- s) yield c.toByte
 
-    new CryptoString(bytes.toList)
+    new CryptoString(bytes.toArray)
   }
 
   def fromHex(s: String) = {
@@ -43,6 +86,8 @@ object CryptoString {
       byte = s.substring(2 * i , 2 * i + 2)
     } yield Integer.valueOf(byte, 16).toByte
 
-    new CryptoString(bytes.toList)
+    new CryptoString(bytes.toArray)
   }
+
+  def md = MessageDigest.getInstance("SHA-256")
 }
